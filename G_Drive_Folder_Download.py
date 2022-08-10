3886ba35239d1272ea5a06df71c911fc232c9c41
Ahmar Zaidi <71930390+AhmarZaidi@users.noreply.github.com>
2022-08-10 16:26:22 +0530

    Update G_Drive_Folder_Download.py

--- G_Drive_Folder_Download.py
+++ G_Drive_Folder_Download.py
@@ -1,135 +1,36 @@
 ############################################################################################################################################################################################################################################################################################
-#Install these libraries(ignore if already installed):
-
-
-#pip install google
-#pip install socket
-#pip install progress
-#pip install --upgrade google-auth
-#pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-
-
-
-############################################################################################################################################################################################################################################################################################
 #Imports
 
 import os
 import io
 import socket
-import pickle
 from time import sleep
 import traceback
 from progress.bar import IncrementalBar
 from googleapiclient.http import MediaIoBaseDownload
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
-from google.auth.transport.requests import Request
-
-import time
 from datetime import datetime as dt
 
-
 from Colors import COLORS
+from Authentication import Create_Service
+from Utility_Functions import sizeScaler, sortFileName, sortFolderFiles, progressPercentage
+from Install_requirements import install_requirements
 
 
 ############################################################################################################################################################################################################################################################################################
 # Generating Client_Secret file, pickle and Creating Service
 
-
-CLIENT_SECRET_FILE = 'client_secret.json' # Place this file (rename the file to 'client_secret.json') in same directory as this script. To get this file follow (https://www.youtube.com/watch?v=6bzzpda63H0). Credits (Channel: https://www.youtube.com/channel/UCvVZ19DRSLIC2-RUOeWx8ug)
-API_NAME = 'drive'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/drive']
-
-def Create_Service(client_secret_file, api_name, api_version, *scopes): # From (Channel: https://www.youtube.com/channel/UCvVZ19DRSLIC2-RUOeWx8ug)
-    CLIENT_SECRET_FILE = client_secret_file
-    API_SERVICE_NAME = api_name
-    API_VERSION = api_version
-    SCOPES = [scope for scope in scopes[0]]
-
-    cred = None
-
-    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
-    # print(pickle_file)
-
-    if os.path.exists(pickle_file):
-        with open(pickle_file, 'rb') as token:
-            cred = pickle.load(token)
-
-    if not cred or not cred.valid:
-        if cred and cred.expired and cred.refresh_token:
-            cred.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-            cred = flow.run_local_server()
-
-        with open(pickle_file, 'wb') as token:
-            pickle.dump(cred, token)
-
-    try:
-        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
-        #print(API_SERVICE_NAME, 'service created successfully')
-        return service
-    except Exception as e:
-        print('Unable to connect.')
-        print(e)
-        return None
-
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-
-
+service = Create_Service()
 
 ############################################################################################################################################################################################################################################################################################
 # Global Variables
-
 
 parentDownloadDirectory = os.getcwd()
 totalNumberOfFiles = 0
 totalSize = 0
 currentFileNumber = 0
-# OS = os.platform.system() # 'Linux', 'Windows', 'Darwin'
 
-
-
-############################################################################################################################################################################################################################################################################################
-# Utility Functions
-
-
-def sortFolderFiles(itemsList):
-    l1 = []
-    l2 = []
-    for x in itemsList:
-        if x['mimeType'] == 'application/vnd.google-apps.folder':
-            l1.append(x)
-        else:
-            l2.append(x)
-    return l1+l2 
-
-def sortFileName(itemsList):
-    return sorted(itemsList, key=lambda x: x['name']) 
-
-
-def progressPercentage(progress):
-    return int(progress*100)
-
-
-def sizeScaler(size): # size in bytes
-    ret = ""
-    if size <= 100: # bytes
-        ret = "["+str(size)+" bytes]" 
-    elif size > 100 and size <= 100000: # Kilobytes
-        ret = "["+str(round((size/1024),2))+" KB]"
-    elif size > 100000 and size <= 1000000000:
-        ret = "["+str(round(size/pow(1024,2),2))+" MB]" #Megabytes
-    elif size > 1000000000:
-        ret = "["+str(round(size/pow(1024,3),2))+" GB]" # Gigabytes
-    return ret
-
-
-
-############################################################################################################################################################################################################################################################################################
-# Main Functions
+# ############################################################################################################################################################################################################################################################################################
+# Download Functions
 
 
 def currentDirectoryLister(folder_id):
@@ -142,6 +43,7 @@ def currentDirectoryLister(folder_id):
         itemsList.extend(response.get('files'))
         nextPageToken = response.get('nextPageToken')
     return itemsList
+
 
 def numberAndSizeOfOfFilesRecursive(id): 
     global totalNumberOfFiles
@@ -161,8 +63,6 @@ def numberAndSizeOfOfFilesRecursive(id):
                     print("Size Error")
 
                 
-            
-
 def downloadFile(file_id, path, size):
     request = service.files().get_media(fileId=file_id)
     fh = io.BytesIO()
@@ -207,7 +107,6 @@ def downloadFolderRecursive(id):
                 folderName = current['name']
                 path = os.path.join(os.getcwd(), folderName)
                 print("\n\n\nFolder:", (path.replace(parentDownloadDirectory,'')).replace(os.sep,' > ')[2:])
-                #print("Path :", path)
                 if not os.path.isdir(path):
                     os.makedirs(path)
                 os.chdir(path)
@@ -237,7 +136,6 @@ def downloadFolder(id):
     downloadFolderRecursive(id)
 
 
-
 ############################################################################################################################################################################################################################################################################################
 # Call Function
 
@@ -247,20 +145,27 @@ def main():
     global totalNumberOfFiles
     global totalSize
     global currentFileNumber
+
+    install_requirements() # Install requirements
     os.system('cls||clear')
+
     print(f"[##########################{COLORS['Yellow']}Google Drive Folder Download Without Zip{COLORS['Reset']}##########################]")
+
     ids = input(f"\nEnter Google Drive Folder Id(s) (separated by comma): {COLORS['Cyan']}")
     idList = [x.replace(" ","") for x in ids.split(",")]
     noOfDownloads = len(idList)
     path = input(f"{COLORS['Reset']} \nEnter destination path (leave empty to create a Downloads folder in current directory): {COLORS['Cyan']}")
+
     if not os.path.isdir(path):
         path = os.path.join(os.getcwd(), "Downloads")
         print(f"{COLORS['Yellow']}\n-Folder (with subfolders and files) Will be Downloaded to the Current Directory.")
     else:
         print(f"{COLORS['Yellow']}\n-Folder (with subfolders and files) Will be Downloaded to: {path}")
+
     print(f"-Note: To stop the download open ['Task Manager'(Windows)/'System Monitor'(Linux)] and")
     print(f"['End Task'/'Stop'] 'python' or 'python3'.{COLORS['Reset']}")
     key = input(f"-Press 'y' to Continue OR 'n' to Quit. {COLORS['Cyan']}")
+
     if key == "y":
         if not os.path.isdir(path):
             os.makedirs(path)
@@ -281,5 +186,4 @@ def main():
     elif key == "n":
         exit()
 
-
-main()    
+main() 
